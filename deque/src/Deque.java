2b8cc5e597a7759b081bb281f46b6024a55ea03d
zhouyiqi <386346852@qq.com>
2019-10-22 16:27:17 +0800

    deque

--- deque/src/Deque.java
+++ deque/src/Deque.java
@@ -1,5 +1,7 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Deque<Item> implements Iterable<Item> {
 
